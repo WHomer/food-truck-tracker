50b2ad2be44872c394cc1a7b542387e82bf79ca7
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-22 20:25:19 -0500

    firebase-push - started seed data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,27 @@
 firebase.initializeApp(config);
+var seedTruck = {
+  truckName : "my Truck",
+  websiteURL : "Google.com",
+  twitterURL : "twitter.com",
+  tags : ["food", "water"],
+  schedule : null,
+  lastSeen : null,
+  posts : [],
+};
+
+var seedPost = {
+  
+}
+
 
 function DBpushTruck(){
   event.preventDefault();
 
     //an object to hold the info to be pushed
-    var obj = {
-    };
+    var truck = seedTruck;
 
     //push the object to the db
-    database.ref("trucks/").push(obj);
+    database.ref("trucks/").push(truck);
     //clear input boxes, if necessary
     //$("#input").val("");
 }
