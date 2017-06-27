38271ca4119a58e04286a0bbca3f4bcf6e16022f
Rebecca Way <rebeccaway2016@u.northwestern.edu>
2017-06-26 22:46:36 -0500

    searchfunction - removed lodash, added moment.js, improved search function, added time-checking function, and added input retrieval to dbpush

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,9 @@ var seedTruck = {
   truckName : "my Truck2",
   websiteURL : "Google.com",
   twitterURL : "twitter.com",
-  truckCuisine : "Food",
+  truckCuisine : "noodles",
   truckSchedule : null,
+  truckDescription: null,
   // lastSeen : null,
   // posts : [],
 };
@@ -16,22 +17,25 @@ var seedTruck = {
 
 function DBpushTruck(){
     //an object to hold the info to be pushed
-    var truck = seedTruck;
+    // var truck = seedTruck;
+    var truckName = ($("#add-truck-name").val()) ? $("#add-truck-name").val().trim(): null;
+    var truckCuisine = ($("#add-truck-cuisine").val()) ? $("#add-truck-cuisine").val().trim(): null;
+    var truckDescription = ($("#add-truck-description").val()) ? $("#add-truck-description").val().trim(): null;
+    var truck = {
+      truckName: truckName,
+      truckCuisine: truckCuisine,
+      truckDescription: truckDescription,
+      truckSchedule : null,
+    };
     //push the object to the db
     database.ref("trucks/").push(truck);
 }
 
-// database.ref("trucks/").once("value", function(snapshot) {
-//   // do some stuff once
-//   var test = _.map(snapshot.val(), "truckName");
-//   console.log(test);
-// });
-
 function retrieveInput(){
   //if there is text in the input field, the var will set to it. otherwise sets to false
-  var truckName = ($("#input-truckName").val()) ? $("#input-truckName").val(): false;
-  var truckCuisine = ($("#input-truckCuisine").val()) ?  $("#input-truckCuisine").val() : false;
-  var truckSchedule = ($("#input-truckSchedule").val()) ? $("#input-truckSchedule").val() : false;
+  var truckName = ($("#truck-name").val()) ? $("#truck-name").val().trim().toLowerCase(): false;
+  var truckCuisine = ($("#truck-cuisine").val()) ?  $("#truck-cuisine").val().trim().toLowerCase() : false;
+  var truckSchedule = ($("#input-truck-date").val()) ? $("#input-truck-date").val().trim().toLowerCase() : false;
   return {
     truckName:truckName,
     truckCuisine:truckCuisine,
@@ -50,21 +54,39 @@ function DBsearch(){
       console.log(truck);
       //for each potential input
       for (var property in inputObj){
-        //if the input exists (and ONLY if the input is present)...
-        if(inputObj[property]){
-          //test if it matches this truck
-          if(inputObj[property]===truck[property]){
-            console.log(inputObj[property]);
+        //if the input and truck property exist (and ONLY if they exist, to avoid comparing to null)...
+        if(inputObj[property] && truck[property]){
+          //test if the property in the input section (inputObj) matches this property of the truck
+          if(inputObj[property].toLowerCase()===truck[property].toLowerCase()){
             validTrucks.push(truck);
+            //if anything of the properties matches, exit the loop, since we already know there's a match
+            break;
           }
         }
       }
     }
-    console.log(validTrucks);
-    // var test2 = _.map(snapshot.val(), "truckName");
-    // var test = _.find(snapshot.val(), function(o) { return o.truckName == name; });
-    // console.log(test);
+    return validTrucks;
   });
+}
+
+function findOpenTrucks(truckOpen, truckClose){
+  var openTimeMoment = moment(truckOpen, "HH:mm");
+  var closeTimeMoment = moment(truckClose, "HH:mm");
+  var currentTimeMoment = moment();
+  var validTrucks = [];
+  database.ref("trucks/").once("value", function(snapshot) {
+    // do some stuff once
+    inputObj = retrieveInput();
+    for (var key in snapshot.val()){
+      var truck = snapshot.val()[key];
+      //NOTE TO SELF: set open/close time based on truck schedule in database
+
+      if(currentTimeMoment.isBetween(openTimeMoment, closeTimeMoment)){
+        validTrucks.push(truck);
+      }
+    }
+  });
+  return validTrucks;
 }
 
 database.ref().on("child_added", function(snapshot) {
@@ -75,21 +97,6 @@ database.ref().on("child_added", function(snapshot) {
   console.log("Errors handled: "+errorObject.code);
 });
 
-function DBremove(key){
-  event.preventDefault();
-    //an object to hold the info to be pushed
-    var obj = {
-    };
-    //push the object to the db
-    database.ref().child(key).remove();
-}
-
-
-database.ref().on("child_removed", function(snapshot){
-  console.log("removed");
-}, function(errorObject){
-  console.log("Errors handled: "+errorObject.code);
-});
 
 
 
@@ -100,6 +107,21 @@ database.ref().on("child_removed", function(snapshot){
 
 
 
+// function DBremove(key){
+//   event.preventDefault();
+//     //an object to hold the info to be pushed
+//     var obj = {
+//     };
+//     //push the object to the db
+//     database.ref().child(key).remove();
+// }
+//
+//
+// database.ref().on("child_removed", function(snapshot){
+//   console.log("removed");
+// }, function(errorObject){
+//   console.log("Errors handled: "+errorObject.code);
+// });
 
 // var seedPost = {
 //   postName: "this Truck",
