58aa75f5756d48aee5849f8c13ed1ee7cf6d641f
Jermzzzzzzz <jcariaga438@robertmorris.edu>
2017-07-02 11:23:12 -0500

    Add files via upload
    Yelp File

---
+++ assets/js/yelpapi.js
@@ -0,0 +1,33 @@
+
+// to test this code use bash, go to the folder and enter "node yelpapi"
+'use strict';
+
+
+
+const yelp = require('yelp-fusion');
+
+
+// Place holders for Yelp Fusion's OAuth 2.0 credentials. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const clientId = 'jBPRDrNg2NHUe1maKfuxBg';
+const clientSecret = 'XWDjn5nsQMcczQSCh0BJDaupZHjd0GiBCFqcjIig0Izsakeb9HjoXx6JKRt5YMrR';
+
+const searchRequest = {
+  term:'Food Truck',
+  location: 'Chicago, IL'
+};
+
+yelp.accessToken(clientId, clientSecret).then(response => {
+  const client = yelp.client(response.jsonBody.access_token);
+
+//Insert food truck name here
+client.reviews('babycakes-chicago').then(response => {
+  console.log(response.jsonBody.reviews[0].text);
+}).catch(e => {
+  console.log(e);
+
+
+
+});
+});
+
