d1243294ab858b09b2a5a46c723c3d7ea1717f3d
WHomer <billyhomeriii@gmail.com>
2017-07-04 13:48:44 -0500

    adding in new features to lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -2,8 +2,10 @@
 $('.js-truck-list').on('click', function(){
 	//display light box
 	$('.js-lightbox').css('display', 'block');
+	var test = this;
+	console.log(this);
 	//populate data
-
+	getLightboxContent();
 })
 
 
@@ -12,3 +14,20 @@ $('.js-lightbox-close').on('click', function(){
 	//close lightbox
 	$('.js-lightbox').css('display', 'none');
 })
+
+
+//gathers information about the truck
+function getLightboxContent(){
+	//store each variable
+	var truckName = truckjson[0]['Truck Name (DBA)'];
+	var truckCuisine = truckjson[0]['Cuisiine Type'];
+	var truckFacebook = truckjson[0]['Facebook'];
+	var truckImage = truckjson[0]['Image URL']
+	var truckSavory = truckjson[0]['Savory'];
+	var truckSweet = truckjson[0]['Sweet'];
+	var truckTwitter = truckjson[0]['Twitter'];
+	var truckURL = truckjson[0]['URL'];
+	//display each variable to lightbox
+	$('.js-lb-name').text(truckName);
+	$('.js-lb-image').attr('src', truckImage);
+}
