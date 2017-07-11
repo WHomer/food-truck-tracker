cb2923d044ce4f01d67f4b8690819ad182c06fdb
WHomer <billyhomeriii@gmail.com>
2017-07-10 20:38:59 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-apiKey: "AIzaSyCIUR0B1T-tpbx_56-AJPee_41fUX5CWOY",
-authDomain: "bootcamp-food-trucks.firebaseapp.com",
-databaseURL: "https://bootcamp-food-trucks.firebaseio.com",
-projectId: "bootcamp-food-trucks",
-storageBucket: "bootcamp-food-trucks.appspot.com",
-messagingSenderId: "87682987679"
-};
